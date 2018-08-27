14c6d35e6dd4668458d70f805bba9fc4d6768f98
Leo Liu <liulong@shinetechchina.com>
2018-08-27 13:24:35 +0800

    text the bootstrap bulit in the project

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 import App from './App'
 import router from './router'
 import store from './store'
 
 import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+window.jQuery = window.$ = require('jquery/dist/jquery.min')
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
