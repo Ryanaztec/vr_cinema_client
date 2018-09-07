e34df20a3d44756f75e1d311916df3ee57a8c47f
Aohang <songaohang@shinetechchina.com>
2018-09-07 16:20:42 +0800

    update the route

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -16,11 +16,6 @@ export default new Router({
       component: require('@/components/vrcinema').default
     },
     {
-      path: '/cinema_resources',
-      name: 'cinema_resources',
-      component: require('@/components/cinemaresources').default
-    },
-    {
       path: '/video_detail',
       name: 'video_detail',
       component: require('@/components/videodetail').default
