dc85febe8f65c6f522a2e5a0cc2ea7f06e22edd7
GeXY <18539369332@163.com>
2018-09-28 19:35:23 +0800

    add downloading progress component

---
+++ src/renderer/components/downloading-progress.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div v-for="bar in bars" class="row mb-1">
+      <div class="col-sm-2">{{ bar.variant }}</div>
+      <div class="col-sm-10 pt-1">
+        <b-progress :value="bar.value"
+                    :variant="bar.variant"
+                    :key="bar.variant"
+                    show-progress
+        ></b-progress>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movieInfo: {
+      default: [],
+      required: false
+    }
+  },
+  data () {
+    return {}
+  },
+  mounted () {},
+  computed: {
+    bars () {
+      let progress = []
+      let seatNumber = ''
+      this.$store.state.movie.subSeatDownloadingStatus.forEach((value, key) => {
+        let completed = 0
+        if (value.movie_id === this.movieInfo.id) {
+          completed = value.stats.total.completed * 2 - 1 > 0 ? value.stats.total.completed * 2 - 1 : 0
+          this.$store.state.seat.seats.forEach((item, index) => {
+            if (item.id === value.seat_id) {
+              seatNumber = item.seat_number.toString()
+            }
+          })
+          progress.push({variant: seatNumber, value: completed})
+        }
+      })
+      return progress
+    }
+  }
+}
+</script>
