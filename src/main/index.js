b77e463cef327dfbbc6e67ce8491e4ab81aad62d
Aohang <songaohang@shinetechchina.com>
2018-09-29 14:25:55 +0800

    add the code of check for update ----- note ( npm install electron-updater )

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import { app, BrowserWindow, ipcMain } from 'electron'
+import { autoUpdater } from 'electron-updater'
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
@@ -35,6 +36,7 @@ function createWindow () {
   mainWindow.on('closed', () => {
     mainWindow = null
   })
+  updateHandle()
 }
 app.on('ready', createWindow)
 
@@ -64,6 +66,50 @@ ipcMain.on('show-window', () => {
 ipcMain.on('orignal-window', () => {
   mainWindow.unmaximize()
 })
+
+function updateHandle () {
+  let message = {
+    error: '检查更新出错',
+    checking: '正在检查更新……',
+    updateAva: '检测到新版本，正在下载……',
+    updateNotAva: '现在使用的就是最新版本，不用更新'
+  }
+  let uploadUrl = 'http://localhost:9528/download/'
+  autoUpdater.setFeedURL(uploadUrl)
+  autoUpdater.on('error', function (error) {
+    sendUpdateMessage(message.error)
+    sendUpdateMessage(error)
+  })
+  autoUpdater.on('checking-for-update', function () {
+    sendUpdateMessage(message.checking)
+  })
+  autoUpdater.on('update-available', function (info) {
+    sendUpdateMessage(message.updateAva)
+  })
+  autoUpdater.on('update-not-available', function (info) {
+    sendUpdateMessage(message.updateNotAva)
+  })
+
+  // 更新下载进度事件
+  autoUpdater.on('download-progress', function (progressObj) {
+    mainWindow.webContents.send('downloadProgress', progressObj)
+  })
+  autoUpdater.on('update-downloaded', function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
+    ipcMain.on('isUpdateNow', (e, arg) => {
+      autoUpdater.quitAndInstall()
+    })
+    mainWindow.webContents.send('isUpdateNow')
+  })
+
+  ipcMain.on('checkForUpdate', () => {
+    autoUpdater.checkForUpdates()
+  })
+}
+
+// 通过main进程发送事件给renderer进程，提示更新信息
+function sendUpdateMessage (text) {
+  mainWindow.webContents.send('message', text)
+}
 /**
  * Auto Updater
  *
